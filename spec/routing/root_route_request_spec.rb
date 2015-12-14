faba223d7593024ae91775449f9af2102c93b53a
LauraKirby <laurakirby26@gmail.com>
2015-12-13 21:21:38 -0800

    remove repetitive code, do not need to write out Rspec object in each spec file

--- spec/routing/root_route_request_spec.rb
+++ spec/routing/root_route_request_spec.rb
@@ -1,9 +1,7 @@
 require 'rails_helper'
 
-RSpec.feature "ContactsIndexRequests", type: :feature do
-  describe 'visiting the root route' do 
-		it "maps to contacts index page" do 
-			expect(get('/')).to route_to("contacts#index")
-		end
+describe 'visiting the root route' do 
+	it "maps to contacts index page" do 
+		expect(get('/')).to route_to("contacts#index")
 	end
 end
