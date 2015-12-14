ec587803361807252620d48d00deaee21a7a9709
LauraKirby <laurakirby26@gmail.com>
2015-12-13 21:20:56 -0800

    remove repetitive code, do not need to write out Rspec object in each spec file

--- spec/features/contacts_index_requests_spec.rb
+++ spec/features/contacts_index_requests_spec.rb
@@ -1,10 +1,8 @@
 require 'rails_helper'
 
-RSpec.feature "ContactsIndexRequests", type: :feature do
-  describe 'visiting the contacts index page' do 
-		it "can see title of application" do 
-			visit "/"
-			expect(page).to have_content 'My Contacts'
-		end
+describe 'visiting the contacts index page' do 
+	it "can see title of application" do 
+		visit "/"
+		expect(page).to have_content 'My Contacts'
 	end
 end
