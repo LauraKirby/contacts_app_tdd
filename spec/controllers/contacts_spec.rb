415e3ab041c3008382cad5d2ac92070b466e1728
LauraKirby <laurakirby26@gmail.com>
2015-12-13 21:27:51 -0800

    fix typo

--- spec/controllers/contacts_spec.rb
+++ spec/controllers/contacts_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 describe 'GET /' do
 	it "renders the contacts index template" do
-	 expec(response).to render_template("index")
+	 expect(response).to render_template("index")
 	end
 end
