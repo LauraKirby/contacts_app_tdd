b0c08f56852271d193643dab643bd938b439b314
LauraKirby <laurakirby26@gmail.com>
2015-12-13 21:37:15 -0800

    modify to better test actual controller

--- spec/controllers/contacts_spec.rb
+++ spec/controllers/contacts_spec.rb
@@ -1,7 +1,12 @@
 require 'rails_helper'
 
-describe 'GET /' do
-	it "renders the contacts index template" do
-	 expect(response).to render_template("index")
-	end
+describe ContactsController, type: :controller do
+
+  describe "GET #index" do
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
+
 end
