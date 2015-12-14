2f299e34f8a1c88bec8315358e9c16e5737014a8
LauraKirby <laurakirby26@gmail.com>
2015-12-13 21:52:33 -0800

    write 'outside' test for adding a contact

--- spec/features/contacts_index_requests_spec.rb
+++ spec/features/contacts_index_requests_spec.rb
@@ -1,8 +1,13 @@
 require 'rails_helper'
 
 describe 'visiting the contacts index page' do 
-	it "can see title of application" do 
+	it "has title of application" do 
 		visit "/"
 		expect(page).to have_content 'My Contacts'
 	end
+
+	it 'user can click the "Add Contact" button' do 
+		visit "/"
+		expect(page).to have_selector(:link_or_button, 'Add Contact')
+	end 
 end
