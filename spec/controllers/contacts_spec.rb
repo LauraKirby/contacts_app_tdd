e037680b4842ed0b745a2198dd90ee47470ab9b1
LauraKirby <laurakirby26@gmail.com>
2015-12-16 14:34:01 -0800

    add test for create new contact button and new contact page, add create method to contacts controller, implement html for the submit button

--- spec/controllers/contacts_spec.rb
+++ spec/controllers/contacts_spec.rb
@@ -2,11 +2,24 @@ require 'rails_helper'
 
 describe ContactsController, type: :controller do
 
-  describe "GET #index" do
+  describe "GET contacts#index" do
     it "returns http success" do
       get :index
       expect(response).to have_http_status(:success)
     end
   end
 
+  describe "GET contacts#new" do
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  #  describe "Post contacts#create" do
+  #   it "returns http success" do
+  #     post :create
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 end
--- spec/features/contacts_new_resquests_spec.rb
+++ spec/features/contacts_new_resquests_spec.rb
@@ -1,9 +1,22 @@
 require 'rails_helper'
 
 describe 'visiting the new contact page' do 
-	it "has a form with a name and email field" do 
+
+ 	before(:each) do
 		visit "/contacts/new"
+	end 
+
+	it "has a form with a name and email field" do 
 		expect(page).to have_field 'Name'
 		expect(page).to have_field 'Email'
 	end
+
+	it "has a button to submit/post a new conact" do 
+		expect(page).to have_button 'Create'
+	end
+
+	# it "shows a list of all contacts created" do
+	# 	# if contact then contact.name
+	# end 
+
 end
